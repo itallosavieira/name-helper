84510c824dd0b002707951ce5e916f3b7c375ae1
itallosavieira <itallo.sa.vieira@gmail.com>
2021-10-24 16:57:32 -0300

    Add initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1 class="text-center">NameHelper</h1>
+      <br/>
+      <h6 class="text-secondary">Name's generator using Vue.js, Graphcl and Node.js</h6>
+    </div>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <h5>Prefixes <span class="badge bg-info">{{ prefixes.length }}</span> </h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="prefix in prefixes" :key="prefix">
+                    {{ prefix }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <h5>Sufixes <span class="badge bg-info">{{ sufixes.length}}</span> </h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="sufix in sufixes" :key="sufix">
+                    {{ sufix }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br/>
+        <h5>Domains <span class="badge bg-info">{{ domains.length }}</span> </h5>
+        <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="domain in domains" :key="domain">
+                    {{ domain }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css"
+import "font-awesome/css/font-awesome.css"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      prefixes: ['A', 'B', 'C'],
+      sufixes: ['D', 'E', 'F'],
+      domains: ['AD', 'AE', 'AF', 'BD', 'BE', 'BF', 'CD', 'CE', 'CF']
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#slogan {
+  margin: 30px;
+}
+
+#main {
+  background: #f1f1f1;
+  padding: 2rem;
 }
 </style>
